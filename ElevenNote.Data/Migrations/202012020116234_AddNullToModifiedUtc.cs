1612adfaff25e53e813591a61091695157c37f23
sellersjt <jonnie.sellers@gmail.com>
2020-12-01 20:33:15 -0500

    Add note controller and index view

---
+++ ElevenNote.Data/Migrations/202012020116234_AddNullToModifiedUtc.cs
@@ -0,0 +1,18 @@
+﻿namespace ElevenNote.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddNullToModifiedUtc : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Note", "ModifiedUtc", c => c.DateTimeOffset(precision: 7));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Note", "ModifiedUtc", c => c.DateTimeOffset(nullable: false, precision: 7));
+        }
+    }
+}
