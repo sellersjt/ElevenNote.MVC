9645cd79405f7d5885fc66099964158881d93e7c
sellersjt <jonnie.sellers@gmail.com>
2020-12-07 19:16:48 -0500

    Add CategoryController and Index view

---
+++ ElevenNote.WebMVC/Controllers/CategoryController.cs
@@ -0,0 +1,30 @@
+﻿using ElevenNote.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElevenNote.WebMVC.Controllers
+{
+    [Authorize]
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            var service = CreateCategoryService();
+            var model = service.GetCategories();
+
+            return View(model);
+        }
+
+        private CategoryService CreateCategoryService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new CategoryService(userId);
+            return service;
+        }
+    }
+}
